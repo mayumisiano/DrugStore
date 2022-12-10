827128e0322a3406af888fe36020dc735b39df4a
Paula Mayumi <mayumi_siano10@hotmail.com>
2022-12-10 20:27:37 -0300

    Feat: controller, model and repository

---
+++ src/main/java/com/farmacia/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.farmacia.farmacia.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
